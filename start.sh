6e2aae74d1757ecff68cc7f770f1ba6fede12358
suxor <suxor@ich.ms>
2015-07-05 13:24:57 +0200

    Update start script

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-echo -e "${SERVER_TOKEN}\0" > /usr/games/.klei/DoNotStarveTogether/server_token.txt
-curl -L -o /usr/games/.klei/DoNotStarveTogether/settings.ini ${SETTINGS_URL}
-/usr/games/dontstarvetogether/bin/dontstarve_dedicated_server_nullrenderer
+echo -e "${SERVER_TOKEN}\0" > /data/DoNotStarveTogether/server_token.txt
+curl -L -o /data/DoNotStarveTogether/settings.ini ${SETTINGS_URL}
+/usr/games/dontstarvetogether/bin/dontstarve_dedicated_server_nullrenderer -persistent_storage_root /data
 
