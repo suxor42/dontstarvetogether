3a530bb88567288b1a15a338d75546a6e7167eb6
suxor <suxor@ich.ms>
2016-06-29 23:18:05 +0200

    Fix dir structure

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 PERSISTENT_STORAGE_ROOT=${PERSISTENT_STORAGE_ROOT:-/usr/games/.klei/}
+CLUSTER=${CLUSTER:-Cluster_1}
 CONF_DIR=${CONF_DIR:-DoNotStarveTogether}
 
 mkdir -p ${PERSISTENT_STORAGE_ROOT}/${CONF_DIR}
-echo -e "${SERVER_TOKEN}\0" > ${PERSISTENT_STORAGE_ROOT}/${CONF_DIR}/cluster_token.txt
-curl -L -o ${PERSISTENT_STORAGE_ROOT}/${CONF_DIR}/settings.ini ${SETTINGS_URL}
+echo -e "${SERVER_TOKEN}\0" > ${PERSISTENT_STORAGE_ROOT}/${CONF_DIR}/${CLUSTER}/cluster_token.txt
+curl -L -o ${PERSISTENT_STORAGE_ROOT}/${CONF_DIR}/${CLUSTER}/settings.ini ${SETTINGS_URL}
 /usr/games/dontstarvetogether/bin/dontstarve_dedicated_server_nullrenderer -persistent_storage_root ${PERSISTENT_STORAGE_ROOT} -conf_dir ${CONF_DIR}
 
