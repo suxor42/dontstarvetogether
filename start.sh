2aa29f02edcb5377064a6baea4abb16a83fc0138
suxor <suxor@ich.ms>
2016-06-29 23:23:01 +0200

    Create dir structure

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ PERSISTENT_STORAGE_ROOT=${PERSISTENT_STORAGE_ROOT:-/usr/games/.klei/}
 CLUSTER=${CLUSTER:-Cluster_1}
 CONF_DIR=${CONF_DIR:-DoNotStarveTogether}
 
-mkdir -p ${PERSISTENT_STORAGE_ROOT}/${CONF_DIR}
+mkdir -p ${PERSISTENT_STORAGE_ROOT}/${CONF_DIR}/${CLUSTER}
 echo -e "${SERVER_TOKEN}\0" > ${PERSISTENT_STORAGE_ROOT}/${CONF_DIR}/${CLUSTER}/cluster_token.txt
 curl -L -o ${PERSISTENT_STORAGE_ROOT}/${CONF_DIR}/${CLUSTER}/settings.ini ${SETTINGS_URL}
 /usr/games/dontstarvetogether/bin/dontstarve_dedicated_server_nullrenderer -persistent_storage_root ${PERSISTENT_STORAGE_ROOT} -conf_dir ${CONF_DIR}
