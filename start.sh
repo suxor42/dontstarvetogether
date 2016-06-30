3e2c43f8c63c392a364c3645271c70574c3f0b87
suxor <suxor@ich.ms>
2016-06-30 10:41:57 +0200

    Fix check

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /bin/bash 
 PERSISTENT_STORAGE_ROOT=${PERSISTENT_STORAGE_ROOT:-/usr/games/.klei}
 CLUSTER=${CLUSTER:-Cluster_1}
 SHARD=${SHARD:-Master}
@@ -7,7 +7,7 @@ set +x
 mkdir -p ${PERSISTENT_STORAGE_ROOT}/${CONF_DIR}/${CLUSTER}/${SHARD}
 echo -e "${SERVER_TOKEN}\0" > ${PERSISTENT_STORAGE_ROOT}/${CONF_DIR}/${CLUSTER}/cluster_token.txt
 curl -L -o ${PERSISTENT_STORAGE_ROOT}/${CONF_DIR}/${CLUSTER}/cluster.ini ${CLUSTER_SETTINGS_URL}
-if [ -z ${ADMIN_LIST_URL+x} ]; then
+if [ -n ${ADMIN_LIST_URL} ]; then
 curl -L -o ${PERSISTENT_STORAGE_ROOT}/${CONF_DIR}/${CLUSTER}/adminlist.txt ${ADMIN_LIST_URL}
 fi
 /usr/games/dontstarvetogether/bin/dontstarve_dedicated_server_nullrenderer \
